6d04930c6b62cff385cb4e4109f91b1ef77020d1
Liubov Murarova <murarova.lubov@gmail.com>
2019-07-16 19:28:31 +0300

    Updates

---
+++ precache-manifest.120a5daafaeb6191e745f90c6d7539fe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56b480e71048858304ebbf67f24f3a84",
+    "url": "/goit-react-hw-04-reader/index.html"
+  },
+  {
+    "revision": "e213fe03b242e9b61bbd",
+    "url": "/goit-react-hw-04-reader/static/css/main.aa976efd.chunk.css"
+  },
+  {
+    "revision": "dcded8253a269e9a3fb7",
+    "url": "/goit-react-hw-04-reader/static/js/2.9132997c.chunk.js"
+  },
+  {
+    "revision": "e213fe03b242e9b61bbd",
+    "url": "/goit-react-hw-04-reader/static/js/main.e54b2613.chunk.js"
+  },
+  {
+    "revision": "84bf89e08b01550b9b7d",
+    "url": "/goit-react-hw-04-reader/static/js/runtime~main.860f1cd1.js"
+  }
+]);
